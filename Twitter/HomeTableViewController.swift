3e37560071e0c1c4eeb34162751667a2db8b16b0
Kusch Qin <flying-withme@hotmail.com>
2022-03-18 16:24:35 -0400

    Added tweet, like, and retweet features

--- Twitter/HomeTableViewController.swift
+++ Twitter/HomeTableViewController.swift
@@ -21,7 +21,9 @@ class HomeTableViewController: UITableViewController {
         
         myRefreshControl.addTarget(self, action: #selector(loadTweets), for:  .valueChanged)
         tableView.refreshControl = myRefreshControl
-        
+        // self.tableView.refreshControl = refresher
+        // self.tableView.rowHeight = UITableView.automaticDimentsion
+        // self.tableView.estimatedRowHeight = 150
     }
     override func viewDidAppear(_ animated: Bool) {
             super.viewDidAppear(animated)
@@ -102,6 +104,10 @@ class HomeTableViewController: UITableViewController {
         if let imageData = data {
             cell.profileImageView.image = UIImage(data: imageData)
         }
+        
+        cell.setFavorite(tweetArray[indexPath.row]["favorited"] as! Bool)
+        cell.tweetId = tweetArray[indexPath.row]["id"] as! Int
+        cell.setRetweeted(tweetArray[indexPath.row]["retweeted"] as! Bool)
         
         return cell
     }
